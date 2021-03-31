504f1a113064371d0fc690f025976632319e95e3
amurray2 <42621202+amurray2@users.noreply.github.com>
2021-03-31 15:49:42 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using BowlingLeague.Models;
+using BowlingLeague.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BowlingLeagueContext context { get; set; }
+
+
+        public HomeController(ILogger<HomeController> logger, BowlingLeagueContext con)
+        {
+            _logger = logger;
+            context = con;
+        }
+
+        [HttpGet]
+        public IActionResult Index(long? teamId, string teamName, int pageNum = 1)
+        {
+            int pageSize = 5;
+
+            return View(new IndexViewModel
+            {
+                Bowlers = context.Bowlers
+                .Where(b => b.TeamId == teamId || teamId == null)
+                .OrderBy(b => b.BowlerFirstName)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize)
+                .ToList(),
+
+                PageNumberingInfo = new PageNumberingInfo
+                {
+                    NumItemsPerPage = pageSize,
+                    CurrentPage = pageNum,
+
+                    //If no team has been selected, then get the full count. Otherwise, only count the number
+                    //from selected team.
+                    TotalNumItems = (teamId == null ? context.Bowlers.Count() : 
+                        context.Bowlers.Where(b => b.TeamId == teamId).Count())
+                },
+                TeamName = teamName
+            });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
